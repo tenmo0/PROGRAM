227d7d17ae38e960ee1e1105ca0afdbfdfcf9905
tenmo0 <khenethsorbito80@gmail.com>
2026-01-01 23:49:47 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/APP-KARINDERYA/", // your repo name
+  base: "/PROGRAM/", // your repo name
   plugins: [react()],
 });
